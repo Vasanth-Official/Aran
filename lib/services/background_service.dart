2199ca5da9acb40590f3ca4157c37c6ec3ef1063
Vasanth-Official <vasanth@example.com>
2026-04-02 08:43:31 +0530

    fix: resolve AndroidServiceInstance unresolved type error in background service

--- lib/services/background_service.dart
+++ lib/services/background_service.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
